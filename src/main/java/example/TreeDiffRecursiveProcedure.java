8ed791667c1f231a76355148536693c53d4d0c70
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-10-05 14:56:51 +0200

    diff: add WinStructField to recursable types

--- src/main/java/example/TreeDiffRecursiveProcedure.java
+++ src/main/java/example/TreeDiffRecursiveProcedure.java
@@ -107,7 +107,8 @@ public class TreeDiffRecursiveProcedure {
     }
 
     private boolean isRecursableLabel(String type) {
-        return "Tree".equals(type) || "WinRegKey".equals(type) || "WinStruct".equals(type);
+        return "Tree".equals(type) || "WinRegKey".equals(type) || "WinStruct".equals(type)
+                || "WinStructField".equals(type);
     }
 
     private Map<String, NodeInfo> collectNodeInfo(Node root, List<String> filter) {
